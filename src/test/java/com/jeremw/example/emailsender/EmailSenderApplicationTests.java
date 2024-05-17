9b93a0a90fa143c615eada90409e82e97aa9e9f6
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2024-05-17 20:46:36 +0200

    ✅ EmailSenderApplicationTests

--- src/test/java/com/jeremw/example/emailsender/EmailSenderApplicationTests.java
+++ src/test/java/com/jeremw/example/emailsender/EmailSenderApplicationTests.java
@@ -3,11 +3,14 @@ package com.jeremw.example.emailsender;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class EmailSenderApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> EmailSenderApplication.main(new String[] {}));
 	}
 
 }
